be8954be3f87f07d9fc8ee25ba0c1966454fdc5b
Nicolas Drebenstedt <drebenstedtn@rki.de>
2023-10-12 12:58:46 +0200

    Remove stale test

--- tests/connector/test_base.py
+++ tests/connector/test_base.py
@@ -1,39 +1,16 @@
-import pytest
-
 from mex.common.connector import (
     BaseConnector,
     ConnectorContext,
     reset_connector_context,
 )
-from mex.common.settings import BaseSettings
-
-
-class DummySettings(BaseSettings):
-    configuration: str = "DEFAULT_123"
-
-
-@pytest.fixture(autouse=True)
-def settings() -> DummySettings:
-    """Load the settings for this pytest session."""
-    return DummySettings.get()
 
 
 class DummyConnector(BaseConnector):
     def __init__(self) -> None:
-        settings = DummySettings.get()
-        self.config = settings.configuration
         self.closed = False
 
     def close(self) -> None:
         self.closed = True
-
-
-def test_connector_infers_correct_settings_class() -> None:
-    settings = DummySettings.get()
-    settings.configuration = "VALUE_456"
-
-    dummy = DummyConnector.get()
-    assert dummy.config == "VALUE_456"
 
 
 def test_connector_enter_returns_self() -> None:
