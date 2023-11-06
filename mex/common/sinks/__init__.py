630dbc87fa505ddbb226533e4acb50fbbb571f94
Nicolas Drebenstedt <drebenstedtn@rki.de>
2023-11-06 12:13:45 +0100

    Break import cycles, update versions

--- mex/common/sinks/__init__.py
+++ mex/common/sinks/__init__.py
@@ -1,10 +1,20 @@
-from enum import Enum
+from mex.common.sinks.backend_api import post_to_backend_api
+from mex.common.sinks.ndjson import write_ndjson
+from mex.common.sinks.public_api import (
+    post_to_public_api,
+    purge_items_from_public_api,
+    purge_models_from_public_api,
+)
+from mex.common.sinks.purge import purge_items, purge_models
+from mex.common.sinks.types import Sink
 
-
-class Sink(Enum):
-    """Configuration to choose where to send outbound data."""
-
-    BACKEND = "backend"
-    GRAPH = "graph"
-    NDJSON = "ndjson"
-    PUBLIC = "public"
+__all__ = (
+    "Sink",
+    "post_to_backend_api",
+    "write_ndjson",
+    "post_to_public_api",
+    "purge_models_from_public_api",
+    "purge_items_from_public_api",
+    "purge_models",
+    "purge_items",
+)
