1f61e69c6339b64d99387e7dd0adc2611aa3d709
Nicolas Drebenstedt <drebenstedtn@rki.de>
2023-11-06 12:59:43 +0100

    Rollback sink imports

--- mex/common/sinks/__init__.py
+++ mex/common/sinks/__init__.py
@@ -1,20 +1,3 @@
-from mex.common.sinks.backend_api import post_to_backend_api
-from mex.common.sinks.ndjson import write_ndjson
-from mex.common.sinks.public_api import (
-    post_to_public_api,
-    purge_items_from_public_api,
-    purge_models_from_public_api,
-)
-from mex.common.sinks.purge import purge_items, purge_models
 from mex.common.sinks.types import Sink
 
-__all__ = (
-    "Sink",
-    "post_to_backend_api",
-    "write_ndjson",
-    "post_to_public_api",
-    "purge_models_from_public_api",
-    "purge_items_from_public_api",
-    "purge_models",
-    "purge_items",
-)
+__all__ = ("Sink",)
