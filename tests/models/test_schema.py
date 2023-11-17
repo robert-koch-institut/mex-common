1b35166c3516225960a08ad8917129132b7c0866
Nicolas Drebenstedt <drebenstedtn@rki.de>
2023-11-17 17:04:40 +0100

    Add comments

--- tests/models/test_schema.py
+++ tests/models/test_schema.py
@@ -71,29 +71,29 @@ def test_required_fields_match_spec(
 )
 def test_field_defs_match_spec(entity_type: str, field_name: str) -> None:  # noqa: C901
     def prepare_field(obj: list[Any] | dict[str, Any]) -> None:  # noqa: C901
-        if isinstance(obj, list):
+        if isinstance(obj, list):  # prepare each item in a list
             for item in obj:
                 prepare_field(item)
             obj[:] = [item for item in obj if item]
             return
-        obj.pop("sameAs", None)
-        obj.pop("subPropertyOf", None)
-        obj.pop("description", None)
-        obj.pop("default", None)
-        if obj.get("type") == "null":
+        obj.pop("sameAs", None)  # not in models
+        obj.pop("subPropertyOf", None)  # not in models
+        obj.pop("description", None)  # not in spec
+        obj.pop("default", None)  # not in spec
+        if obj.get("type") == "null":  # pydantic 2 sets these
             obj.pop("type")
-        if obj.get("examples") == [str(Identifier.generate(seed=42))]:
+        if obj.get("examples") == [str(Identifier.generate(seed=42))]:  # not in spec
             obj.pop("examples")
-        if field_name == "temporal":
-            obj.pop("examples", None)  # i can't handle this
-        if obj.get("format") == "date":
+        if field_name == "temporal":  # this field's examples are too complicated
+            obj.pop("examples", None)
+        if obj.get("format") == "date":  # we mash up dates and datetimes in models
             obj["format"] = "date-time"
         if obj.get("pattern") in (
             r"^[1-9]\d{3}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$",
-            r"^\d{4}(-\d{2})?(-\d{2})?$",
+            r"^\d{4}(-\d{2})?(-\d{2})?$",  # ignore date/time pattern differences
         ):
             obj["pattern"] = r"^\d{4}(-\d{2}(-\d{2}(T\d{2}:\d{2}:\d{2}Z)?)?)?$"
-        if obj.get("pattern") == MEX_ID_PATTERN:
+        if obj.get("pattern") == MEX_ID_PATTERN:  # align concept/enum annotations
             obj.pop("pattern")
             obj.pop("type")
             if obj.get("title") and field_name not in ("identifier", "stableTargetId"):
@@ -104,7 +104,7 @@ def test_field_defs_match_spec(entity_type: str, field_name: str) -> None:  # no
                 obj.pop("title", None)
         else:
             obj.pop("title", None)
-        if "$ref" in obj:
+        if "$ref" in obj:  # align reference paths
             obj["$ref"] = obj["$ref"].replace("#/$defs/", "/schema/fields/").lower()
             if obj["$ref"] == "/schema/entities/concept#/identifier" and (
                 scheme := obj.pop("useScheme", None)
@@ -112,17 +112,17 @@ def test_field_defs_match_spec(entity_type: str, field_name: str) -> None:  # no
                 name = scheme.replace("-", "").removeprefix("https://mex.rki.de/item/")
                 obj["$ref"] = f"/schema/fields/{name}"
         if obj.get("type") == "array":
-            obj.pop("default", None)
+            obj.pop("default", None)  # ignore empty list defaults
             if isinstance(obj["items"], dict):
                 obj["items"] = [obj["items"]]
-            if scheme := obj.pop("useScheme", None):
+            if scheme := obj.pop("useScheme", None):  # pull down scheme into items
                 for item in obj["items"]:
                     item["useScheme"] = scheme
             prepare_field(obj["items"])
             if any(
                 item.get("examples") if isinstance(item, dict) else None
                 for item in obj["items"]
-            ):
+            ):  # pull up examples onto field level
                 obj["examples"] = [
                     e
                     for item in obj["items"]
@@ -130,22 +130,16 @@ def test_field_defs_match_spec(entity_type: str, field_name: str) -> None:  # no
                         item.pop("examples", []) if isinstance(item, dict) else []
                     )
                 ]
-        if "anyOf" in obj:
+        if "anyOf" in obj:  # ignore any/one difference
             obj["oneOf"] = obj.pop("anyOf")
-        if "oneOf" in obj:
-            prepare_field(obj["oneOf"])
-            obj["oneOf"] = list(
-                json.loads(s) for s in {json.dumps(o) for o in obj["oneOf"]}
-            )
-            if len(obj["oneOf"]) == 1:
-                obj.update(obj.pop("oneOf")[0])
-        if "allOf" in obj:
-            prepare_field(obj["allOf"])
-            obj["allOf"] = list(
-                json.loads(s) for s in {json.dumps(o) for o in obj["allOf"]}
-            )
-            if len(obj["allOf"]) == 1:
-                obj.update(obj.pop("allOf")[0])
+        for quantifier in ("oneOf", "allOf"):
+            if quantifier in obj:
+                prepare_field(obj[quantifier])  # prepare choices
+                obj[quantifier] = list(
+                    json.loads(s) for s in {json.dumps(o) for o in obj[quantifier]}
+                )  # deduplicate items, used for date/times
+                if len(obj[quantifier]) == 1:  # collapse non-choices
+                    obj.update(obj.pop(quantifier)[0])
 
     generated_properties = GENERATED_SCHEMAS[entity_type]["properties"]
     specified_properties = SPECIFIED_SCHEMAS[entity_type]["properties"]
