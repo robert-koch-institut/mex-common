ae2a797253a3e9bb7c8b710dbd97905fd5a52895
Nicolas Drebenstedt <drebenstedtn@rki.de>
2023-11-22 10:05:55 +0100

    Fix docstrings in ldap.extract

--- mex/common/ldap/extract.py
+++ mex/common/ldap/extract.py
@@ -12,18 +12,18 @@ def _get_merged_ids_by_attribute(
     persons: Iterable[LDAPPerson],
     primary_source: ExtractedPrimarySource,
 ) -> dict[str, list[PersonID]]:
-    """Return a mapping from a dynamic Person attribute to the merged IDs.
+    """Return a mapping from a dynamic Person attribute to corresponding PersonIDs.
 
-    Merged IDs are looked up in the identity table and will be omitted
-    for any person that has not yet been transformed and indexed there.
+    PersonIDs are looked up in the identity provider and will be omitted
+    for any person that has not yet been assigned an `Identity` there.
 
     Args:
         attribute: The key to use for the resulting mapping
-        persons: Iterable of LDP persons
+        persons: Iterable of LDAP persons
         primary_source: Primary source for LDAP
 
     Returns:
-        Mapping from `LDAPPerson[attribute]` to corresponding `Identity.stableTargetId`
+        Mapping from a stringified `LDAPPerson[attribute]` to corresponding PersonIDs
     """
     if attribute not in LDAPPerson.model_fields:
         raise RuntimeError(f"Not a valid LDAPPerson field: {attribute}")
@@ -43,17 +43,17 @@ def _get_merged_ids_by_attribute(
 def get_merged_ids_by_employee_ids(
     persons: Iterable[LDAPPerson], primary_source: ExtractedPrimarySource
 ) -> dict[str, list[PersonID]]:
-    """Return a mapping from person's employeeID to the merged IDs.
+    """Return a mapping from a person's employeeID to their PersonIDs.
 
-    Merged IDs are looked up in the identity table and will be omitted
-    for any person that has not yet been transformed and indexed there.
+    PersonIDs are looked up in the identity provider and will be omitted
+    for any person that has not yet been assigned an `Identity` there.
 
     Args:
-        persons: Iterable of LDP persons
+        persons: Iterable of LDAP persons
         primary_source: Primary source for LDAP
 
     Returns:
-        Mapping from `LDAPPerson.employeeID` to corresponding `Identity.stableTargetId`
+        Mapping from `LDAPPerson.employeeID` to corresponding PersonIDs
     """
     return _get_merged_ids_by_attribute("employeeID", persons, primary_source)
 
@@ -61,17 +61,17 @@ def get_merged_ids_by_employee_ids(
 def get_merged_ids_by_email(
     persons: Iterable[LDAPPerson], primary_source: ExtractedPrimarySource
 ) -> dict[str, list[PersonID]]:
-    """Return a mapping from person's e-mail to the merged IDs.
+    """Return a mapping from a person's e-mail to their PersonIDs.
 
-    Merged IDs are looked up in the identity table and will be omitted
-    for any person that has not yet been transformed and indexed there.
+    PersonIDs are looked up in the identity provider and will be omitted
+    for any person that has not yet been assigned an `Identity` there.
 
     Args:
         persons: Iterable of LDP persons
         primary_source: Primary source for LDAP
 
     Returns:
-        Mapping from `LDAPPerson.mail` to corresponding `Identity.stableTargetId`
+        Mapping from `LDAPPerson.mail` to corresponding PersonIDs
     """
     return _get_merged_ids_by_attribute("mail", persons, primary_source)
 
@@ -80,18 +80,17 @@ def get_merged_ids_by_query_string(
     persons_with_query: Iterable[LDAPPersonWithQuery],
     primary_source: ExtractedPrimarySource,
 ) -> dict[str, list[PersonID]]:
-    """Return a mapping from an author query string to the resolved merged IDs.
+    """Return a mapping from a person query string to their PersonIsD.
 
-    Merged IDs are looked up in the identity table and will be omitted
-    for any person that has not yet been transformed and indexed there.
+    PersonIDs are looked up in the identity provider and will be omitted
+    for any person that has not yet been assigned an `Identity` there.
 
     Args:
         persons_with_query: Iterable of LDP persons with query
         primary_source: Primary source for LDAP
 
     Returns:
-        Mapping from `LDAPPersonWithQuery.query` to corresponding
-        `Identity.stableTargetId`
+        Mapping from `LDAPPersonWithQuery.query` to corresponding PersonIDs
     """
     merged_ids_by_attribute = defaultdict(list)
     provider = get_provider()
