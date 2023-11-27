fe6e5e3e1508fc07c36229676a8eae6ad0a14239
Carolin Scholl <schollc@rki.de>
2023-11-27 15:38:26 +0100

    Add backward-compatibility for path resolving

--- mex/common/types/path.py
+++ mex/common/types/path.py
@@ -1,5 +1,7 @@
 import sys
-from pathlib import PosixPath, WindowsPath
+from pathlib import Path, PosixPath, WindowsPath
+from typing import Any
+from warnings import warn
 
 if sys.platform == "win32":
     path_type = WindowsPath
@@ -7,9 +9,22 @@ else:
     path_type = PosixPath
 
 
-class AssetsPath(path_type):
+class _DeprecatedResolvedPath(path_type):
+    """Class to support the deprecated .resolve() and .raw() methods."""
+
+    def resolve(self, *args: Any, **kwargs: Any) -> Path:  # type: ignore
+        """Return absolute path."""
+        warn("deprecated", DeprecationWarning)
+        return super().resolve(*args, **kwargs)
+
+
+class AssetsPath(
+    _DeprecatedResolvedPath
+):  # TODO: inherit from path_type instead after removal of deprecated class
     """Custom path for settings that can be absolute or relative to `assets_dir`."""
 
 
-class WorkPath(path_type):
+class WorkPath(
+    _DeprecatedResolvedPath
+):  # TODO: inherit from path_type instead after removal of deprecated class
     """Custom path for settings that can be absolute or relative to `work_dir`."""
