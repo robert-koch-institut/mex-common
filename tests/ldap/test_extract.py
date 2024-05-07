1f022bb83c61e272016db91bfa86904aadb6835e
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-05-07 12:53:04 +0200

    parametrize

--- tests/ldap/test_extract.py
+++ tests/ldap/test_extract.py
@@ -1,13 +1,21 @@
+import pytest
+
 from mex.common.ldap.extract import _get_merged_ids_by_attribute
 from mex.common.ldap.models.person import LDAPPerson
 from mex.common.models import ExtractedPrimarySource
+from mex.common.types import MergedPersonIdentifier
 
 
+@pytest.mark.parametrize(
+    ("attribute", "expected"), [("sAMAccountName", {})], ids=["person not found"]
+)
 def test_get_merged_ids_by_attribute(
-    extracted_primary_sources: dict[str, ExtractedPrimarySource]
+    attribute: str,
+    expected: dict[str, list[MergedPersonIdentifier]],
+    extracted_primary_sources: dict[str, ExtractedPrimarySource],
 ) -> None:
     merged_ids_by_attribute = _get_merged_ids_by_attribute(
-        "sAMAccountName",
+        attribute,
         [
             LDAPPerson(
                 objectGUID="3f5a722d-e7c1-4c7c-ac40-23ce7753ce40",
@@ -18,4 +26,4 @@ def test_get_merged_ids_by_attribute(
         ],
         extracted_primary_sources["ldap"],
     )
-    assert merged_ids_by_attribute == {}
+    assert merged_ids_by_attribute == expected
