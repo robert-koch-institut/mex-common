f1b5d750eaba536f3f9a0b72bc0f6f8d829410b0
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-05-07 14:27:14 +0200

    include merged_id in expected data

--- tests/ldap/test_extract.py
+++ tests/ldap/test_extract.py
@@ -9,7 +9,7 @@ from mex.common.ldap.extract import (
 )
 from mex.common.ldap.models.person import LDAPPerson
 from mex.common.models import ExtractedPrimarySource
-from mex.common.testing import Joker
+from mex.common.types import Identifier
 
 
 @pytest.fixture
@@ -20,35 +20,66 @@ def ldap_primary_source(
 
 
 @pytest.fixture
-def ldap_persons() -> list[LDAPPerson]:
+def ldap_person_with_identity(
+    ldap_primary_source: ExtractedPrimarySource,
+) -> LDAPPerson:
+    person = LDAPPerson(
+        objectGUID=UUID(int=1, version=4),
+        employeeID="1",
+        givenName=["Anna"],
+        sn="Has Identity",
+    )
+    provider = get_provider()
+    provider.assign(ldap_primary_source.stableTargetId, str(person.objectGUID))
+    return person
+
+
+@pytest.fixture
+def ldap_person_without_identity() -> LDAPPerson:
+    return LDAPPerson(
+        objectGUID=UUID(int=2, version=4),
+        employeeID="2",
+        givenName=["Berta"],
+        sn="Without Identity",
+    )
+
+
+@pytest.fixture
+def ldap_persons(
+    ldap_person_with_identity: LDAPPerson, ldap_person_without_identity: LDAPPerson
+) -> list[LDAPPerson]:
     return [
-        LDAPPerson(
-            objectGUID=UUID(int=1, version=4),
-            employeeID="1",
-            givenName=["Anna"],
-            sn="Has Identity",
-        ),
-        LDAPPerson(
-            objectGUID=UUID(int=2, version=4),
-            employeeID="2",
-            givenName=["Berta"],
-            sn="Without Identity",
-        ),
+        ldap_person_with_identity,
+        ldap_person_without_identity,
     ]
+
+
+@pytest.fixture
+def merged_id_of_person_with_identity(
+    ldap_person_with_identity: LDAPPerson,
+    ldap_primary_source: ExtractedPrimarySource,
+) -> Identifier:
+    provider = get_provider()
+    identities = provider.fetch(
+        had_primary_source=ldap_primary_source.stableTargetId,
+        identifier_in_primary_source=str(ldap_person_with_identity.objectGUID),
+    )
+    return identities[0].stableTargetId
 
 
 def test_get_merged_ids_by_attribute(
     ldap_persons: list[LDAPPerson],
     ldap_primary_source: ExtractedPrimarySource,
+    merged_id_of_person_with_identity: Identifier,
 ) -> None:
-    provider = get_provider()
-    provider.assign(ldap_primary_source.stableTargetId, str(ldap_persons[0].objectGUID))
     merged_ids_by_attribute = _get_merged_ids_by_attribute(
         "sn",
         ldap_persons,
         ldap_primary_source,
     )
-    assert merged_ids_by_attribute == {"Has Identity": [Joker()]}
+    assert merged_ids_by_attribute == {
+        "Has Identity": [merged_id_of_person_with_identity]
+    }
 
     with pytest.raises(RuntimeError):
         _get_merged_ids_by_attribute(
@@ -59,6 +90,15 @@ def test_get_merged_ids_by_attribute(
 
 
 def test_get_merged_ids_by_employee_ids(
-    ldap_persons: list[LDAPPerson], ldap_primary_source: ExtractedPrimarySource
+    ldap_persons: list[LDAPPerson],
+    ldap_primary_source: ExtractedPrimarySource,
+    merged_id_of_person_with_identity: Identifier,
+    ldap_person_with_identity: LDAPPerson,
 ) -> None:
-    get_merged_ids_by_employee_ids(ldap_persons, ldap_primary_source)
+    expected = {
+        ldap_person_with_identity.employeeID: [merged_id_of_person_with_identity]
+    }
+    merged_ids_by_employee_ids = get_merged_ids_by_employee_ids(
+        ldap_persons, ldap_primary_source
+    )
+    assert merged_ids_by_employee_ids == expected
