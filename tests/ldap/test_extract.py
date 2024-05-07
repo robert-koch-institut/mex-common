a1e2ce2d341fb6eac124f70fac2dd5c7c7899d44
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2024-05-07 14:00:45 +0200

    add basic test for get_merged_ids_by_employee_ids

--- tests/ldap/test_extract.py
+++ tests/ldap/test_extract.py
@@ -3,16 +3,25 @@ from uuid import UUID
 import pytest
 
 from mex.common.identity import get_provider
-from mex.common.ldap.extract import _get_merged_ids_by_attribute
+from mex.common.ldap.extract import (
+    _get_merged_ids_by_attribute,
+    get_merged_ids_by_employee_ids,
+)
 from mex.common.ldap.models.person import LDAPPerson
 from mex.common.models import ExtractedPrimarySource
 from mex.common.testing import Joker
 
 
-def test_get_merged_ids_by_attribute(
-    extracted_primary_sources: dict[str, ExtractedPrimarySource],
-) -> None:
-    persons = [
+@pytest.fixture
+def ldap_primary_source(
+    extracted_primary_sources: dict[str, ExtractedPrimarySource]
+) -> ExtractedPrimarySource:
+    return extracted_primary_sources["ldap"]
+
+
+@pytest.fixture
+def ldap_persons() -> list[LDAPPerson]:
+    return [
         LDAPPerson(
             objectGUID=UUID(int=1, version=4),
             employeeID="1",
@@ -26,12 +35,17 @@ def test_get_merged_ids_by_attribute(
             sn="Without Identity",
         ),
     ]
-    ldap_primary_source = extracted_primary_sources["ldap"]
+
+
+def test_get_merged_ids_by_attribute(
+    ldap_persons: list[LDAPPerson],
+    ldap_primary_source: ExtractedPrimarySource,
+) -> None:
     provider = get_provider()
-    provider.assign(ldap_primary_source.stableTargetId, str(persons[0].objectGUID))
+    provider.assign(ldap_primary_source.stableTargetId, str(ldap_persons[0].objectGUID))
     merged_ids_by_attribute = _get_merged_ids_by_attribute(
         "sn",
-        persons,
+        ldap_persons,
         ldap_primary_source,
     )
     assert merged_ids_by_attribute == {"Has Identity": [Joker()]}
@@ -39,6 +53,12 @@ def test_get_merged_ids_by_attribute(
     with pytest.raises(RuntimeError):
         _get_merged_ids_by_attribute(
             "foo",
-            persons,
+            ldap_persons,
             ldap_primary_source,
         )
+
+
+def test_get_merged_ids_by_employee_ids(
+    ldap_persons: list[LDAPPerson], ldap_primary_source: ExtractedPrimarySource
+) -> None:
+    get_merged_ids_by_employee_ids(ldap_persons, ldap_primary_source)
