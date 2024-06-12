6a6fbff5c6c496b60def0c2780d69a88c7616bab
Nicolas Drebenstedt <drebenstedtn@rki.de>
2024-06-12 10:03:14 +0200

    Fix ndjson test

--- tests/sinks/test_ndjson.py
+++ tests/sinks/test_ndjson.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Literal
 from uuid import UUID
 
 from pydantic import UUID4
@@ -6,15 +7,20 @@ from pydantic import UUID4
 from mex.common.models import ExtractedData
 from mex.common.settings import BaseSettings
 from mex.common.sinks.ndjson import write_ndjson
-from mex.common.types import Identifier, TemporalEntity
+from mex.common.types import (
+    ExtractedIdentifier,
+    Identifier,
+    MergedIdentifier,
+    TemporalEntity,
+)
 
 
 class DummyEnum(Enum):
     NAME = "value"
 
 
-class Thing(ExtractedData):
-    identifier: Identifier
+class Thing(ExtractedData[ExtractedIdentifier, MergedIdentifier]):
+    entityType: Literal["Thing"] = "Thing"
     str_attr: str
     enum_attr: DummyEnum | None = None
     uuid_attr: UUID4 | None = None
@@ -25,29 +31,42 @@ def test_write_ndjson() -> None:
     settings = BaseSettings.get()
 
     test_models = [
-        Thing.model_construct(identifier="1", str_attr="foo"),
-        Thing.model_construct(identifier="2", str_attr="bar", enum_attr=DummyEnum.NAME),
-        Thing.model_construct(
-            identifier="3", str_attr="baz", uuid_attr=UUID(int=42, version=4)
+        Thing(
+            hadPrimarySource=Identifier.generate(41),
+            identifierInPrimarySource="1",
+            str_attr="foo",
         ),
-        Thing.model_construct(
-            identifier="4", str_attr="dat", ts_attr=TemporalEntity(2000, 1, 1)
+        Thing(
+            hadPrimarySource=Identifier.generate(42),
+            identifierInPrimarySource="2",
+            str_attr="bar",
+            enum_attr=DummyEnum.NAME,
+        ),
+        Thing(
+            hadPrimarySource=Identifier.generate(43),
+            identifierInPrimarySource="3",
+            str_attr="baz",
+            uuid_attr=UUID(int=42, version=4),
+        ),
+        Thing(
+            hadPrimarySource=Identifier.generate(44),
+            identifierInPrimarySource="4",
+            str_attr="dat",
+            ts_attr=TemporalEntity(2000, 1, 1),
         ),
     ]
 
     ids = list(write_ndjson(test_models))
-    assert len(ids)
+    assert ids == [m.identifier for m in test_models]
 
     with open(settings.work_dir / "Thing.ndjson") as handle:
         output = handle.read()
 
-    expected = """\
-{{"enum_attr": null, "identifier": "{}", "str_attr": "foo", "ts_attr": null, "uuid_attr": null}}
-{{"enum_attr": "value", "identifier": "{}", "str_attr": "bar", "ts_attr": null, "uuid_attr": null}}
-{{"enum_attr": null, "identifier": "{}", "str_attr": "baz", "ts_attr": null, "uuid_attr": "00000000-0000-4000-8000-00000000002a"}}
-{{"enum_attr": null, "identifier": "{}", "str_attr": "dat", "ts_attr": "2000-01-01", "uuid_attr": null}}
-""".format(
-        *[m.identifier for m in test_models]
+    assert (
+        output
+        == """{"entityType": "Thing", "enum_attr": null, "hadPrimarySource": "bFQoRhcVH5DHU5", "identifier": "eBBkb544HJi9Fuvr6Ogf27", "identifierInPrimarySource": "1", "stableTargetId": "g1zzwSdgfOfcimsSRtIKja", "str_attr": "foo", "ts_attr": null, "uuid_attr": null}
+{"entityType": "Thing", "enum_attr": "value", "hadPrimarySource": "bFQoRhcVH5DHU6", "identifier": "ulVMQQ87fDrv3O6WAhgo3", "identifierInPrimarySource": "2", "stableTargetId": "fSOS56LUmJz4MSDUKwUAVr", "str_attr": "bar", "ts_attr": null, "uuid_attr": null}
+{"entityType": "Thing", "enum_attr": null, "hadPrimarySource": "bFQoRhcVH5DHU7", "identifier": "faPTaLe0LHG2w0izPjuTP3", "identifierInPrimarySource": "3", "stableTargetId": "fbEI02h2ASTxd441B9fyxr", "str_attr": "baz", "ts_attr": null, "uuid_attr": "00000000-0000-4000-8000-00000000002a"}
+{"entityType": "Thing", "enum_attr": null, "hadPrimarySource": "bFQoRhcVH5DHU8", "identifier": "cEDoKScSEuGPbo4jFD5cfd", "identifierInPrimarySource": "4", "stableTargetId": "eeyCEYnqXC8xKgPPrlK1HJ", "str_attr": "dat", "ts_attr": "2000-01-01", "uuid_attr": null}
+"""
     )
-
-    assert output == expected
