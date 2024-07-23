8a0745dba3d3a2d25c3d74053a7c7a81db2776c6
Kamran Ali <33874616+mr-kamran-ali@users.noreply.github.com>
2024-07-23 16:36:23 +0200

    Update mex/common/models/base.py
    Co-authored-by: erichesse <hessee@rki.de>
    Signed-off-by: Kamran Ali <33874616+mr-kamran-ali@users.noreply.github.com>

--- mex/common/models/base.py
+++ mex/common/models/base.py
@@ -125,7 +125,7 @@ class BaseModel(PydanticBaseModel):
         """Build a cached list of fields can be set to None."""
         field_names: list[str] = []
         for field_name, field_info in cls.get_all_fields().items():
-            validator = TypeAdapter(field_info.annotation)  # type: ignore[var-annotated]
+            validator: TypeAdapter[Any] = TypeAdapter(field_info.annotation)
             try:
                 validator.validate_python(None)
             except ValidationError:
