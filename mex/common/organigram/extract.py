5bbe5683e39c888d0d49c6e8796045c9d8e2d372
Janina Esins <esinsj@rki.de>
2024-11-07 18:15:05 +0100

    throw MExError for label and email conflicts.

--- mex/common/organigram/extract.py
+++ mex/common/organigram/extract.py
@@ -1,6 +1,7 @@
 import json
 from collections.abc import Generator, Iterable
 
+from mex.common.exceptions import MExError
 from mex.common.logging import watch
 from mex.common.models import ExtractedOrganizationalUnit
 from mex.common.organigram.models import OrganigramUnit
@@ -51,6 +52,7 @@ def get_unit_merged_ids_by_synonyms(
     """Return a mapping from unit alt_label and label to their merged IDs.
 
     There will be multiple entries per unit mapping to the same merged ID.
+    But if the same entry mapps to differnt merged unit IDs, an error is thrown.
 
     Args:
         extracted_units: Iterable of extracted units
@@ -58,11 +60,20 @@ def get_unit_merged_ids_by_synonyms(
     Returns:
         Mapping from unit synonyms to stableTargetIds
     """
-    return {
-        synonym: MergedOrganizationalUnitIdentifier(extracted_unit.stableTargetId)
-        for extracted_unit in extracted_units
-        for synonym in _get_synonyms(extracted_unit)
-    }
+    synonym_dict: dict[str, MergedOrganizationalUnitIdentifier] = {}
+    for extracted_unit in extracted_units:
+        for synonym in _get_synonyms(extracted_unit):
+            if (
+                synonym in synonym_dict
+                and synonym_dict[synonym] != extracted_unit.stableTargetId
+            ):
+                msg = (
+                    f"Conflict: label '{synonym}' is associated with merged unit ID "
+                    f"{synonym_dict[synonym]} and {extracted_unit.stableTargetId}."
+                )
+                raise MExError(msg)
+            synonym_dict[synonym] = extracted_unit.stableTargetId
+    return synonym_dict
 
 
 def get_unit_merged_ids_by_emails(
@@ -71,6 +82,7 @@ def get_unit_merged_ids_by_emails(
     """Return a mapping from unit emails to their merged IDs.
 
     There may be multiple emails per unit mapping to the same merged ID.
+    But if the same email mapps to differnt merged unit IDs, an error is thrown.
 
     Args:
         extracted_units: Iterable of extracted units
@@ -78,8 +90,18 @@ def get_unit_merged_ids_by_emails(
     Returns:
         Mapping from lowercased `email` to stableTargetIds
     """
-    return {
-        email.lower(): MergedOrganizationalUnitIdentifier(extracted_unit.stableTargetId)
-        for extracted_unit in extracted_units
-        for email in extracted_unit.email
-    }
+    email_dict: dict[str, MergedOrganizationalUnitIdentifier] = {}
+    for extracted_unit in extracted_units:
+        for email in extracted_unit.email:
+            lower_email = email.lower()
+            if (
+                lower_email in email_dict
+                and email_dict[lower_email] != extracted_unit.stableTargetId
+            ):
+                msg = (
+                    f"Conflict: email {email} is associated with merged unit ID "
+                    f"{email_dict[lower_email]} and {extracted_unit.stableTargetId}."
+                )
+                raise MExError(msg)
+            email_dict[lower_email] = extracted_unit.stableTargetId
+    return email_dict
