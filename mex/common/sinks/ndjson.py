b3ae8b74cfb360ee1196a3dc7dc89951f6055ba1
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2025-01-28 11:33:58 +0100

    accept rules in ndjson sink

--- mex/common/sinks/ndjson.py
+++ mex/common/sinks/ndjson.py
@@ -2,10 +2,10 @@ import json
 from collections.abc import Generator, Iterable
 from contextlib import ExitStack
 from pathlib import Path
-from typing import IO, Any
+from typing import IO, Any, overload
 
 from mex.common.logging import logger
-from mex.common.models import AnyExtractedModel
+from mex.common.models import AnyExtractedModel, AnyRuleModel
 from mex.common.settings import BaseSettings
 from mex.common.sinks.base import BaseSink
 from mex.common.transform import MExEncoder
@@ -27,26 +27,38 @@ class NdjsonSink(BaseSink):
     def close(self) -> None:
         """Nothing to close, since load already closes all file handles."""
 
+    @overload
     def load(
         self,
-        models: Iterable[AnyExtractedModel],
-    ) -> Generator[AnyExtractedIdentifier, None, None]:
-        """Write models into a new-line delimited JSON file.
+        models_or_rules: Iterable[AnyExtractedModel],
+    ) -> Generator[AnyExtractedIdentifier, None, None]: ...
+
+    @overload
+    def load(
+        self,
+        models_or_rules: Iterable[AnyRuleModel],
+    ) -> None: ...
+
+    def load(
+        self,
+        models_or_rules: Iterable[AnyExtractedModel] | Iterable[AnyRuleModel],
+    ) -> Generator[AnyExtractedIdentifier, None, None] | None:
+        """Write models or rules into a new-line delimited JSON file.
 
         Args:
-            models: Iterable of extracted models to write
+          models_or_rules: Iterable of extracted models or rules to write
 
         Returns:
-            Generator for identifiers of written models
+          Generator for identifiers of written models or None if rules.
         """
         file_handles: dict[str, IO[Any]] = {}
         total_count = 0
         with ExitStack() as stack:
-            for chunk in grouper(self.CHUNK_SIZE, models):
-                for model in chunk:
-                    if model is None:
+            for chunk in grouper(self.CHUNK_SIZE, models_or_rules):
+                for model_or_rule in chunk:
+                    if model_or_rule is None:
                         continue
-                    class_name = model.__class__.__name__
+                    class_name = model_or_rule.__class__.__name__
                     try:
                         fh = file_handles[class_name]
                     except KeyError:
@@ -59,7 +71,11 @@ class NdjsonSink(BaseSink):
                             class_name,
                             file_name.as_posix(),
                         )
-                    fh.write(f"{json.dumps(model, sort_keys=True, cls=MExEncoder)}\n")
+                    fh.write(
+                        f"{json.dumps(model_or_rule, sort_keys=True, cls=MExEncoder)}\n"
+                    )
                     total_count += 1
-                    yield model.identifier
-                logger.info("%s - written %s models", type(self).__name__, total_count)
+                    if hasattr(model_or_rule, "identifier"):
+                        yield model_or_rule.identifier
+                logger.info("%s - written %s items", type(self).__name__, total_count)
+        return None
