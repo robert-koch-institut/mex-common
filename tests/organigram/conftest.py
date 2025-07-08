4c258e11e36564bfc5e8c4e5b27d2c438c3c30d5
Zehra Ciftci <CiftciZ@rki.de>
2025-07-08 10:30:05 +0200

    add rki_organization and set unitOf

--- tests/organigram/conftest.py
+++ tests/organigram/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 
 from mex.common.models import ExtractedOrganizationalUnit, ExtractedPrimarySource
+from mex.common.models.organization import ExtractedOrganization
 from mex.common.organigram.models import OrganigramUnit
 from mex.common.organigram.transform import (
     transform_organigram_units_to_organizational_units,
 )
 from mex.common.types import Link, LinkLanguage, Text
+from mex.common.types.identifier import MergedPrimarySourceIdentifier
 
 
 @pytest.fixture
@@ -31,10 +33,11 @@ def child_unit() -> OrganigramUnit:
 def extracted_child_unit(
     child_unit: OrganigramUnit,
     extracted_primary_sources: dict[str, ExtractedPrimarySource],
+    rki_organization: ExtractedOrganization,
 ) -> ExtractedOrganizationalUnit:
     """Return the child unit transformed to an ExtractedOrganizationalUnit."""
     return transform_organigram_units_to_organizational_units(
-        [child_unit], extracted_primary_sources["organigram"]
+        [child_unit], extracted_primary_sources["organigram"], rki_organization
     )[0]
 
 
@@ -62,8 +65,19 @@ def parent_unit() -> OrganigramUnit:
 def extracted_parent_unit(
     parent_unit: OrganigramUnit,
     extracted_primary_sources: dict[str, ExtractedPrimarySource],
+    rki_organization: ExtractedOrganization,
 ) -> ExtractedOrganizationalUnit:
     """Return the parent unit transformed to an ExtractedOrganizationalUnit."""
     return transform_organigram_units_to_organizational_units(
-        [parent_unit], extracted_primary_sources["organigram"]
+        [parent_unit], extracted_primary_sources["organigram"], rki_organization
     )[0]
+
+
+@pytest.fixture
+def rki_organization() -> ExtractedOrganization:
+    """Return a mock RKI organization for testing."""
+    return ExtractedOrganization(
+        officialName=[Text(value="Robert Koch-Institut")],
+        hadPrimarySource=MergedPrimarySourceIdentifier("RKIID1234567890"),
+        identifierInPrimarySource="RKIID9876543210",
+    )
