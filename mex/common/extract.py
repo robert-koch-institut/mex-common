b1610f4fb0c65f650402c77d452c13f3f2a58520
Mikail Kerim Yaldiz <57573182+MikaMk4@users.noreply.github.com>
2025-10-16 16:58:50 +0200

    feature/mx-2032 Summarize logging of parse_csv (#549)
    ### Changes
    - Instead of logging them individually, parse_csv now summarizes error
    logs in batches with the count of pydantic error_types and also logs the
    total amount of successful validations.
    
    ---------
    
    Signed-off-by: Mikail Kerim Yaldiz <57573182+MikaMk4@users.noreply.github.com>
    Co-authored-by: erichesse <hessee@rki.de>

--- mex/common/extract.py
+++ mex/common/extract.py
@@ -41,6 +41,7 @@ def parse_csv(
     path_or_buffer: Union[str, PathLike[str], "ReadCsvBuffer[Any]"],
     into: type[_BaseModelT],
     chunksize: int = 10000,
+    summary_batch_size: int = 10000,
     **kwargs: Any,  # noqa: ANN401
 ) -> Generator[_BaseModelT, None, None]:
     """Parse a CSV file into an iterable of the given model type.
@@ -49,11 +50,16 @@ def parse_csv(
         path_or_buffer: Location of CSV file or read buffer with CSV content
         into: Type of model to parse
         chunksize: Buffer size for chunked reading
+        summary_batch_size: Batch size for summary logs
         kwargs: Additional keywords arguments for pandas
 
     Returns:
         Generator for models
     """
+    error_summary: defaultdict[str, int] = defaultdict(int)
+    total_rows_processed = 0
+    total_rows_successfully_processed = 0
+
     with pd.read_csv(
         path_or_buffer,
         chunksize=chunksize,
@@ -66,19 +72,38 @@ def parse_csv(
                 into.__name__,
                 i,
             )
-            for index, row in chunk.iterrows():
+            for _, row in chunk.iterrows():
                 try:
                     model = into.model_validate(
                         row.replace(to_replace=np.nan, value=None)
                         .replace(regex=r"^\s*$", value=None)
                         .to_dict()
                     )
+                    total_rows_successfully_processed += 1
                     yield model
                 except ValidationError as error:
+                    for validation_error in error.errors():
+                        error_type = validation_error["type"]
+                        error_summary[error_type] += 1
+
+                total_rows_processed += 1
+
+                if total_rows_processed % summary_batch_size == 0 and error_summary:
                     logger.error(
-                        "parse_csv - %s %s - %s - %s",
-                        into.__name__,
-                        index,
-                        error.__class__.__name__,
-                        error.errors(),
+                        "Summarizing errors for batch with rows %s to %s",
+                        total_rows_processed - summary_batch_size + 1,
+                        total_rows_processed,
                     )
+                    for error_type, count in error_summary.items():
+                        logger.error(
+                            " - Error type '%s': %s occurrences", error_type, count
+                        )
+                    error_summary.clear()
+
+    if error_summary:
+        logger.error("Summarizing errors for remaining rows")
+        for error_type, count in error_summary.items():
+            logger.error(" - Error type '%s': %s occurrences", error_type, count)
+        logger.info(
+            "Successfully processed %s items.", total_rows_successfully_processed
+        )
