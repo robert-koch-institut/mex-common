3df044823bb154c2a0d0b02921cc54cc3a2151f1
Zehra Ciftci <CiftciZ@rki.de>
2026-01-29 16:17:32 +0100

    renaming

--- tests/backend_api/conftest.py
+++ tests/backend_api/conftest.py
@@ -23,7 +23,7 @@ def mocked_backend(monkeypatch: MonkeyPatch) -> MagicMock:
 
 @pytest.fixture
 def mocked_ldap_backend(monkeypatch: MonkeyPatch) -> MagicMock:
-    """Return the mocked request dispatch method of backend connector."""
+    """Return the mocked request dispatch method of LDAP backend connector."""
     mocked_send_request = MagicMock(
         spec=LDAPBackendApiConnector._send_request,
         return_value=Mock(json=MagicMock(return_value={})),
