b8beedeebb0ab0d6e571ab21537b96625dc8cb18
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2026-03-19 10:48:50 +0100

    add test for get_extracted_organizational_unit_with_parents

--- tests/organigram/test_helpers.py
+++ tests/organigram/test_helpers.py
@@ -1,5 +1,11 @@
-from mex.common.organigram.helpers import build_child_map, find_descendants
+from mex.common.models import ExtractedOrganizationalUnit
+from mex.common.organigram.helpers import (
+    build_child_map,
+    find_descendants,
+    get_extracted_organizational_unit_with_parents,
+)
 from mex.common.organigram.models import OrganigramUnit
+from mex.common.types import MergedOrganizationIdentifier, MergedPrimarySourceIdentifier
 
 
 def test_build_child_map(
@@ -21,3 +27,18 @@ def test_find_descendants(
     )
 
     assert child_ids == [child_unit.identifier]
+
+
+def test_get_extracted_organizational_unit_with_parents(
+    extracted_child_unit: ExtractedOrganizationalUnit,
+    extracted_parent_unit: ExtractedOrganizationalUnit,
+    extracted_primary_source_ids: dict[str, MergedPrimarySourceIdentifier],
+    rki_organization_id: MergedOrganizationIdentifier,
+) -> None:
+    expected = {extracted_child_unit, extracted_parent_unit}
+    returned = get_extracted_organizational_unit_with_parents(
+        "CHLD Unterabteilung",
+        extracted_primary_source_ids["organigram"],
+        rki_organization_id,
+    )
+    assert set(returned) == expected
