aff9fa460d43bf48f86a75e625356aebcb5c0fbd
Eric Heße <hessee@rki.de>
2026-07-31 09:44:08 +0200

    test full path

--- tests/assets/test_filesystem.py
+++ tests/assets/test_filesystem.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 from mex.common.assets.filesystem import FilesystemAssetsConnector
@@ -14,7 +16,9 @@ def test_filesystem_read() -> None:
 def test_glob() -> None:
     connector = FilesystemAssetsConnector.get()
     returned = connector.glob("raw-data/organigram", "*.*")
-    assert returned[0].endswith("organizational_units.json")
+    assert returned == [
+        str(Path.cwd() / "assets/raw-data/organigram/organizational_units.json")
+    ]
 
 
 def test_connector_only_allows_existing_path() -> None:
